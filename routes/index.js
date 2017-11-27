9b144bb95a6db160c21f32032140de402f4076ae
Qiyuan Xu <qiyuan.xu6@gmail.com>
2017-11-27 14:09:37 -0500

    before group meeting

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,47 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 // Connect string to MySQL
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'fling.seas.upenn.edu',
-  user     : 'minghw',
-  password : 'azure1902',
-  database : 'minghw'
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'project-550-1.cn5iz4eo9j7i.us-east-1.rds.amazonaws.com:3306',
+  user     : 'Project550',
+  password : 'Project550',
+  database : 'mydb',
+});
+
+//Connect to Mlab
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://Project550:Project550.@ds113566.mlab.com:13566/mydb', (err) => {
+  if (err) {
+    console.log('Could not connect to Mlab: ', err);
+  } else {
+    console.log('Connected to Mlab!')
+  }
+});
+
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+  id: { type: String, required: true, unqiue: true},
+  name: { type: String, required: true},
+  followedPlayers: [{type: String}],
+  followedTeams: [{type: String}],
+  myTeam: {
+    GK: {type: String, default: 0},
+    CB1: {type: String, default: 0},
+    CB2: {type: String, default: 0},
+    LB: {type: String, default: 0},
+    RB: {type: String, default: 0},
+    CM1: {type: String, default: 0},
+    CM2: {type: String, default: 0},
+    LM: {type: String, default: 0},
+    RM: {type: String, default: 0},
+    CF1: {type: String, default: 0},
+    CF2: {type: String, default: 0}
+  }
 });
 
 /* GET home page. */
@@ -16,50 +49,6 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
 });
 
-router.get('/reference', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../', 'views', 'reference.html'));
-});
 
-router.get('/insert', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../', 'views', 'insert.html'));
-});
-
-router.get('/data/:email', function(req,res) {
-  // use console.log() as print() in case you want to debug, example below:
-  // console.log("inside person email");
-  var query = 'SELECT Person.login, Person.name, Person.sex, Person.relationshipStatus, Person.birthyear, f.num FROM Person LEFT JOIN (SELECT login, COUNT(*) AS num FROM Friends GROUP BY login) f on Person.login = f.login';
-  // you may change the query during implementation
-  var email = req.params.email;
-  if (email != 'undefined') query = query + ' where Person.login ="' + email + '"' ;
-  console.log(query);
-  connection.query(query, function(err, rows, fields) {
-    if (err) console.log(err);
-    else {
-        res.json(rows);
-    }
-    });
-});
-
-
-// ----Your implemention of route handler for "Insert a new record" should go here-----
-router.get('/ins',function(req,res){
-  var login = req.query.login;
-  var name = req.query.name;
-  var sex = req.query.sex;
-  var relationshipStatus = req.query.RelationshipStatus;
-  var birthyear = req.query.Birthyear;
-
-console.log(login);
-if (login != 'undefined')
-  var query = 'INSERT INTO Person(login,name,sex,relationshipStatus,birthyear) VALUES ("'+login+'","'+name+'","'+sex+'","'+relationshipStatus+'","'+birthyear+'")';
-  console.log(query);
-  connection.query(query, function(err, rows, fields) {
-    if (err) console.log(err);
-    else {
-        res.json(rows);
-        console.log(success);
-    }
-    });
-});
 
 module.exports = router;
