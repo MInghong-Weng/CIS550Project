8d3297e025ed33f510cf8c0820cbadd23b78ec04
Hai Cao <imcaohai@gmail.com>
2017-11-29 17:52:26 -0500

    playerSearch router order change

--- routes/index.js
+++ routes/index.js
@@ -69,7 +69,7 @@ router.get('/playerSearch/age/:playerAge', function(req, res) {
 
   console.log(req.params);
 
-  var query = "select p.name, p.club, p.age, p.nationality, p.overall from mydb.PlayerPersonalData p where p.age = 20";
+  var query = "select p.name, p.club, p.age, p.nationality, p.overall from mydb.PlayerPersonalData p where p.age = 20 order by p.overall desc";
   console.log(query);
   connection.query(query, function(err, rows, fields) {
     if (err) console.log(err);
