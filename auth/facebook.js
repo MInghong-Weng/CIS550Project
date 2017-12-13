aba935a14ec38c1d8f6f59c4962f25aab292dc66
Qiyuan Xu <qiyuan.xu6@gmail.com>
2017-12-12 22:41:48 -0500

    port

--- auth/facebook.js
+++ auth/facebook.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 passport.use(new FacebookStrategy({
   clientID: "132409940757832",
   clientSecret: "f5b32cc6e9b085b7d426955d3e99e48a",
-  callbackURL: "http://127.0.0.1:8080/auth/facebook/callback"
+  callbackURL: "http://ec2-34-239-126-102.compute-1.amazonaws.com:3000/auth/facebook/callback"
 },
 
 function(accessToken, refreshToken, profile, done) {
